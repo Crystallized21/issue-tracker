4a46f23b7ba77a670665d7c52e03df9e322dca4f
Michael Bui <michaellol345@outlook.com>
2024-06-17 23:46:46 +1200

    moved inline function

--- app/components/ErrorMessage.tsx
+++ app/components/ErrorMessage.tsx
@@ -1,9 +1,6 @@
-import React, {PropsWithChildren, ReactNode} from 'react';
+import React, {PropsWithChildren} from 'react';
 import {Text} from "@radix-ui/themes";
 
-interface Props {
-    children: ReactNode;
-}
 
 const ErrorMessage = ({ children }: PropsWithChildren) => {
     if (!children) return null;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -23,6 +23,17 @@ const NewIssuePage = () => {
     const [error, setError] = useState('');
     const [isSubmitting, setSubmitting] = useState(false);
 
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setSubmitting(false);
+            setError('An unexpected error occurred. Please try again.');
+        }
+    })
+
     return (
         <div className="max-w-xl">
             {error && <Callout.Root color="red" className="mb-5">
@@ -30,16 +41,8 @@ const NewIssuePage = () => {
             </Callout.Root>}
             <form
                 className="space-y-3"
-                onSubmit={handleSubmit(async (data) => {
-                    try {
-                        setSubmitting(true);
-                        await axios.post('/api/issues', data);
-                        router.push('/issues');
-                    } catch (error) {
-                        setSubmitting(false);
-                        setError('An unexpected error occurred. Please try again.');
-                    }
-                })}>
+                onSubmit={onSubmit}
+            >
                 <TextField.Root placeholder="Title" {...register('title')}/>
                 <ErrorMessage>
                     {errors.title?.message}
