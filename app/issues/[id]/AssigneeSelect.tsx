416ca718c57d2cf59329f933e7f649fc9dc06c8f
Michael Bui <michael.bui_crystal@outlook.com>
2024-06-23 21:24:20 +1200

    built filter componnetnte

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -47,7 +47,6 @@ const AssigneeSelect = ({ issue }: {issue: Issues}) => {
     );
 };
 
-//e
 const useUsers = () => useQuery<User[]>({
     queryKey: ['users'],
     queryFn: () => axios.get('/api/users').then(res => res.data),
