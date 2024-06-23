e8fbc054893e55f88bc7e54b2ae31f4fff5567f5
Michael Bui <michaellol345@outlook.com>
2024-06-23 16:34:30 +1200

    help

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -9,30 +9,28 @@ import {Skeleton} from "@/app/components";
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: {issue: Issues}) => {
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000,// 60s
-        retry: 3
-    });
+    const {data: users, error, isLoading} = useUsers();
 
     if (isLoading) return <Skeleton/>
 
     if (error) return null;
 
+    const assignIssue = (userId: string) => {
+        axios
+            .patch('/api/issues/' + issue.id, {
+                assignedToUserId: userId === 'unassigned' ? null : userId
+            })
+            .catch(() => {
+                toast.error('Changes could not be saved. Please try again.');
+            });
+    }
+
     return (
         <>
             <Select.Root
                 defaultValue={issue.assignedToUserId || ""}
-                onValueChange={(userId) => {
-                axios
-                    .patch('/api/issues/' + issue.id, {
-                    assignedToUserId: userId === 'unassigned' ? null : userId
-                    })
-                    .catch(() => {
-                        toast.error('Changes could not be saved. Please try again.');
-                    });
-            }}>
+                onValueChange={assignIssue}
+            >
                 <Select.Trigger placeholder="Assign..."/>
                     <Select.Content>
                         <Select.Group>
@@ -48,5 +46,12 @@ const AssigneeSelect = ({ issue }: {issue: Issues}) => {
         </>
     );
 };
+
+const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3
+})
 
 export default AssigneeSelect;
